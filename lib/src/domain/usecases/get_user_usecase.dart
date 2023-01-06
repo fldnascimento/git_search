1adcac816dec08fa7669a932db503bd91107959f
Felipe Nascimento <felipeliman@hotmail.com>
2023-01-06 14:34:19 -0300

    created usecase get user

---
+++ lib/src/domain/usecases/get_user_usecase.dart
@@ -0,0 +1,26 @@
+import 'package:result_dart/result_dart.dart';
+
+import '../entities/user_entity.dart';
+import '../errors/errors.dart';
+import '../repositories/git_hub_repository.dart';
+
+abstract class GetUserUsecase {
+  AsyncResult<UserEntity, Error> call({required String login});
+}
+
+class GetUserUsecaseImpl implements GetUserUsecase {
+  final GitHubRepository gitHubRepository;
+
+  const GetUserUsecaseImpl({
+    required this.gitHubRepository,
+  });
+
+  @override
+  AsyncResult<UserEntity, Error> call({required String login}) async {
+    if (login.isEmpty) {
+      return Failure(ParamEmpty());
+    }
+
+    return gitHubRepository.getUser(login: login);
+  }
+}
---
+++ test/src/domain/usecases/get_user_usecase_test.dart
@@ -0,0 +1,40 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:git_search/src/domain/usecases/get_user_usecase.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:result_dart/result_dart.dart';
+
+import 'package:git_search/src/domain/errors/errors.dart';
+import 'package:git_search/src/domain/repositories/git_hub_repository.dart';
+
+class GitHubRepositoryMock extends Mock implements GitHubRepository {}
+
+void main() {
+  late GitHubRepositoryMock repository;
+  late GetUserUsecaseImpl usecase;
+
+  setUp(() {
+    repository = GitHubRepositoryMock();
+    usecase = GetUserUsecaseImpl(gitHubRepository: repository);
+  });
+
+  test('should call repository.getUser', () async {
+    when(() => repository.getUser(login: any(named: 'login')))
+        .thenAnswer((_) async => Failure(UserNotFound()));
+    await usecase(login: 'fldnascimento');
+    verify(() => repository.getUser(login: any(named: 'login'))).called(1);
+  });
+
+  test('should return a user not found', () async {
+    when(() => repository.getUser(login: any(named: 'login')))
+        .thenAnswer((_) async => Failure(UserNotFound()));
+    final result = await usecase(login: 'fldnascimento');
+    expect(result.fold((s) => s, (f) => f), isA<UserNotFound>());
+  });
+
+  test('should return a ParamEmpty when query is empty', () async {
+    when(() => repository.getUser(login: any(named: 'login')))
+        .thenAnswer((_) async => Failure(ParamEmpty()));
+    final result = await usecase(login: '');
+    expect(result.fold((s) => s, (f) => f), isA<ParamEmpty>());
+  });
+}

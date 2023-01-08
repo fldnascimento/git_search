bd530e77edef0a1a1c9df67e694810007c6a7f2f
Felipe Nascimento <felipeliman@hotmail.com>
2023-01-08 20:03:50 -0300

    added state failure test on list users cubit

--- test/src/presenter/cubits/list_users/list_users_cubit_test.dart
+++ test/src/presenter/cubits/list_users/list_users_cubit_test.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:git_search/src/domain/errors/errors.dart';
+import 'package:git_search/src/domain/errors/errors_messages.dart';
 import 'package:git_search/src/domain/usecases/find_users_usecase.dart';
 import 'package:git_search/src/infra/models/user_model.dart';
 import 'package:git_search/src/presenter/cubits/list_users/list_users_cubit.dart';
@@ -21,12 +23,89 @@ void main() {
     listUsersCubit = ListUsersCubit(findUsersUsecase: findUsersUsecase);
   });
 
-  test('should find users', () async {
+  test('should emit state ListUsersLoadingState', () async {
+    when(() => findUsersUsecase(query: any(named: 'query')))
+        .thenAnswer((_) async => const Failure(UsersNotFound()));
+    listUsersCubit.findUsers('fldnascimento');
+    expect(listUsersCubit.state, ListUsersLoadingState());
+  });
+
+  test('should emit state ListUsersSuccessState', () async {
     final user = UserModel.fromMap(jsonDecode(userJson));
-    when(() => findUsersUsecase(query: 'fldnascimento'))
+    when(() => findUsersUsecase(query: any(named: 'query')))
         .thenAnswer((_) async => Success(<UserModel>[user]));
 
     await listUsersCubit.findUsers('fldnascimento');
     expect(listUsersCubit.state, ListUsersSuccessState(users: [user]));
   });
+
+  test(
+    'should emit state ListUsersFailureState when users not found',
+    () async {
+      when(() => findUsersUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(UsersNotFound()));
+
+      await listUsersCubit.findUsers('fldnascimento');
+      expect(
+        listUsersCubit.state,
+        const ListUsersFailureState(message: ErrorsMessages.usersNotFound),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListUsersFailureState when rate limit exceeded',
+    () async {
+      when(() => findUsersUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(RateLimitExceeded()));
+
+      await listUsersCubit.findUsers('fldnascimento');
+      expect(
+        listUsersCubit.state,
+        const ListUsersFailureState(message: ErrorsMessages.rateLimitExceeded),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListUsersFailureState when timeout connection',
+    () async {
+      when(() => findUsersUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(TimeoutConnection()));
+
+      await listUsersCubit.findUsers('fldnascimento');
+      expect(
+        listUsersCubit.state,
+        const ListUsersFailureState(message: ErrorsMessages.timeoutConnection),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListUsersFailureState when empty param',
+    () async {
+      when(() => findUsersUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(ParamEmpty()));
+
+      await listUsersCubit.findUsers('fldnascimento');
+      expect(
+        listUsersCubit.state,
+        const ListUsersFailureState(message: ErrorsMessages.paramEmpty),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListUsersFailureState when error unknown',
+    () async {
+      when(() => findUsersUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(ErrorUnknown()));
+
+      await listUsersCubit.findUsers('fldnascimento');
+      expect(
+        listUsersCubit.state,
+        const ListUsersFailureState(message: ErrorsMessages.errorUnknown),
+      );
+    },
+  );
 }

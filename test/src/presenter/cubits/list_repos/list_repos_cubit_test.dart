84c0b05878296408da22adb97143745da98e23d0
Felipe Nascimento <felipeliman@hotmail.com>
2023-01-08 20:24:35 -0300

    added state failure test on list repos cubit

--- test/src/presenter/cubits/list_repos/list_repos_cubit_test.dart
+++ test/src/presenter/cubits/list_repos/list_repos_cubit_test.dart
@@ -1,6 +1,8 @@
 import 'dart:convert';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:git_search/src/domain/errors/errors.dart';
+import 'package:git_search/src/domain/errors/errors_messages.dart';
 import 'package:git_search/src/domain/usecases/find_repos_usecase.dart';
 import 'package:git_search/src/infra/models/repo_model.dart';
 import 'package:git_search/src/presenter/cubits/list_repos/list_repos_cubit.dart';
@@ -21,7 +23,14 @@ void main() {
     listReposCubit = ListReposCubit(findReposUsecase: findReposUsecase);
   });
 
-  test('should find repos', () async {
+  test('should emit state ListReposLoadingState', () async {
+    when(() => findReposUsecase(query: any(named: 'query')))
+        .thenAnswer((_) async => const Failure(ReposNotFound()));
+    listReposCubit.findRepos('fldnascimento');
+    expect(listReposCubit.state, ListReposLoadingState());
+  });
+
+  test('should emit state ListReposSuccessState', () async {
     final repo = RepoModel.fromMap(jsonDecode(repoJson));
     when(() => findReposUsecase(query: 'fldnascimento'))
         .thenAnswer((_) async => Success(<RepoModel>[repo]));
@@ -29,4 +38,74 @@ void main() {
     await listReposCubit.findRepos('fldnascimento');
     expect(listReposCubit.state, ListReposSuccessState(repos: [repo]));
   });
+
+  test(
+    'should emit state ListReposFailureState when repos not found',
+    () async {
+      when(() => findReposUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(ReposNotFound()));
+
+      await listReposCubit.findRepos('fldnascimento');
+      expect(
+        listReposCubit.state,
+        const ListReposFailureState(message: ErrorsMessages.reposNotFound),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListReposFailureState when rate limit exceeded',
+    () async {
+      when(() => findReposUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(RateLimitExceeded()));
+
+      await listReposCubit.findRepos('fldnascimento');
+      expect(
+        listReposCubit.state,
+        const ListReposFailureState(message: ErrorsMessages.rateLimitExceeded),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListReposFailureState when timeout connection',
+    () async {
+      when(() => findReposUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(TimeoutConnection()));
+
+      await listReposCubit.findRepos('fldnascimento');
+      expect(
+        listReposCubit.state,
+        const ListReposFailureState(message: ErrorsMessages.timeoutConnection),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListReposFailureState when empty param',
+    () async {
+      when(() => findReposUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(ParamEmpty()));
+
+      await listReposCubit.findRepos('fldnascimento');
+      expect(
+        listReposCubit.state,
+        const ListReposFailureState(message: ErrorsMessages.paramEmpty),
+      );
+    },
+  );
+
+  test(
+    'should emit state ListReposFailureState when error unknown',
+    () async {
+      when(() => findReposUsecase(query: any(named: 'query')))
+          .thenAnswer((_) async => const Failure(ErrorUnknown()));
+
+      await listReposCubit.findRepos('fldnascimento');
+      expect(
+        listReposCubit.state,
+        const ListReposFailureState(message: ErrorsMessages.errorUnknown),
+      );
+    },
+  );
 }

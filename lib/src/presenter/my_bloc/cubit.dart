7dd8451d9a9a481c3153c9dfc9c5fa27fd0ec1d7
Felipe Nascimento <felipeliman@hotmail.com>
2023-01-09 03:30:49 -0300

    adjusted emit cubit for web

--- lib/src/presenter/my_bloc/cubit.dart
+++ lib/src/presenter/my_bloc/cubit.dart
@@ -1,12 +1,13 @@
 import 'dart:io';
 
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
 abstract class Cubit<T> extends ValueNotifier<T> {
   Cubit(super.value);
 
   emit(T state) {
-    if (Platform.environment.containsKey('FLUTTER_TEST')) {
+    if (!kIsWeb && Platform.environment.containsKey('FLUTTER_TEST')) {
       value = state;
     } else {
       WidgetsBinding.instance.addPostFrameCallback((_) {
